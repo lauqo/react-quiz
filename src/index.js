54244250a905d877c24c915f82a539afbd01977d
Laura <laura.kaliyeva@nu.edu.kz>
2020-05-11 16:16:50 +0600

    add redux

--- src/index.js
+++ src/index.js
@@ -2,12 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App';
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
 import {BrowserRouter} from 'react-router-dom'
 import * as serviceWorker from './serviceWorker'
+import rootReducer from './store/reducers/rootReducer';
 
-const app = <BrowserRouter>
-    <App />
-</BrowserRouter>;
+const store = createStore(rootReducer);
+
+const app = (
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
+);
 
 ReactDOM.render(
   <React.StrictMode>
