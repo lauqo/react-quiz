93394af79effe00e51077270d6998540745ec807
Laura <laura.kaliyeva@nu.edu.kz>
2020-05-11 16:20:32 +0600

    add redux-thunk and configure redux devtools

--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App';
-import {createStore} from 'redux'
+import {applyMiddleware, compose, createStore} from 'redux'
 import {Provider} from 'react-redux'
 import {BrowserRouter} from 'react-router-dom'
 import * as serviceWorker from './serviceWorker'
+import reduxThunk from 'redux-thunk'
 import rootReducer from './store/reducers/rootReducer';
 
-const store = createStore(rootReducer);
+const composeEnhancers =
+    typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        }) : compose;
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(reduxThunk)));
 
 const app = (
     <Provider store={store}>
